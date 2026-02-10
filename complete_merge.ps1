4686c1f3aa75c7160a65a00b3ed74622ce4b22a9
Chris Fisher <ctf5791@gmail.com>
2026-02-10 07:18:55 -0500

    Undo blueberry changes and remove complete_merge.ps1

--- complete_merge.ps1
+++
@@ -1,5 +0,0 @@
-$env:GIT_EDITOR = 'true'
-cd "c:\Users\ctf57\OneDrive\Desktop\WvNatureWeatherApp"
-git commit --no-edit
-git push origin main
-Remove-Item "c:\Users\ctf57\OneDrive\Desktop\WvNatureWeatherApp\complete_merge.ps1" -ErrorAction SilentlyContinue
